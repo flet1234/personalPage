5876a8a37f82412242dbceceb02b4d122faf90df
flet1234 <flet1908@gmail.com>
2024-06-10 10:23:06 +0300

    fix

--- portfolio_react_tsc/src/components/CV.tsx
+++ portfolio_react_tsc/src/components/CV.tsx
@@ -3,7 +3,7 @@ import cv from '../assets/Maksim Gold CV FULL-STACK.pdf'
 const CV = () => {
     return (
         <div>
-            <iframe style={{width:'vw%', height:'100vh'}} src={cv}/>
+            <iframe style={{width:'100vw', height:'100vh'}} src={cv}/>
         </div>
     )
 }
